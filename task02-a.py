bff927df4018fb8cc0da57e09ce33f3cdcca64cc
Mathias Bøhn Grytemark <mathias@grytemark.no>
2017-12-02 23:22:22 +0100

    move colors to generic module

--- task02-a.py
+++ task02-a.py
@@ -2,16 +2,8 @@
 
 import csv
 
+import func
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 INPUTFILE = './task02-a.input'
 checksum = 0
@@ -23,7 +15,7 @@ with open(INPUTFILE, mode='r') as csvfile:
         row = list(map(int, row))
         print(row)
         rowdiff = int(max(row)) - int(min(row))
-        print("{}current checksum: {}\t|max: {}\t| min: {}\t| diff: {}\t| new checksum: {}{}".format(bcolors.BOLD, checksum, max(row), min(row), rowdiff, checksum + rowdiff, bcolors.ENDC))
+        print("{}current checksum: {}\t|max: {}\t| min: {}\t| diff: {}\t| new checksum: {}{}".format(func.bcolors.BOLD, checksum, max(row), min(row), rowdiff, checksum + rowdiff, func.bcolors.ENDC))
         checksum = checksum + rowdiff
 
 print("checksum is: {}".format(checksum))
